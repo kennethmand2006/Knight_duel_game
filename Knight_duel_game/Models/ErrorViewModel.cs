b57bfa8b29ee75807075143a7e02a24a2fff021d
Opilane TTHK <opilane@tthk.ee>
2024-11-15 10:26:22 +0200

    init

---
+++ Knight_duel_game/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Knight_duel_game.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
